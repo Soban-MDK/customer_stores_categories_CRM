2438910b8035929a134d7dbf89bcc918b5449b0a
Soban-MDK <mohammad.s@medkart.in>
2025-03-03 15:25:11 +0530

    Add: Error Handling and logging

--- scripts/New_customers.py
+++ scripts/New_customers.py
@@ -5,6 +5,7 @@ import numpy as np
 from db.common_helpers import get_data
 from db.connections import get_db_engine_pos
 from db.queries import NEW_CUSTOMERS_QUERY
+from utils.logger import logger, logging
 
 new_category_values = {
     '0_To_3_Lacs': 15,                  
@@ -30,31 +31,46 @@ def assign_category(data, store_category_data):
 
 def filter_based_on_category(data):
     """Filters the data based on the category and removes the remaining data"""
-    
-    conditions = [
-        (data['category'] == '0_To_3_Lacs') & ((data['bill_count'].between(1, 5)) | (data['AOV'] > 50)),
-        (data['category'] == '3_To_6_Lacs') & ((data['bill_count'].between(1, 5)) | (data['AOV'] > 100)),
-        (data['category'] == 'Greater_Than_6_Lacs') & ((data['bill_count'].between(1, 5)) | (data['AOV'] > 200))
-    ]
+    try:
+        conditions = [
+            (data['category'] == '0_To_3_Lacs') & ((data['bill_count'].between(1, 5)) | (data['AOV'] > 50)),
+            (data['category'] == '3_To_6_Lacs') & ((data['bill_count'].between(1, 5)) | (data['AOV'] > 100)),
+            (data['category'] == 'Greater_Than_6_Lacs') & ((data['bill_count'].between(1, 5)) | (data['AOV'] > 200))
+        ]
 
-    filtered_data = data[conditions[0] | conditions[1] | conditions[2]]
-    return filtered_data
+        filtered_data = data[conditions[0] | conditions[1] | conditions[2]]
+        return filtered_data
+
+    except Exception as e:
+        logging()
+        return
 
 
 def final_data_limited(filtered_data):
-    filtered_data = filtered_data.sort_values(['category', 'LTV', 'bill_count'], ascending=[True, False])
+    try:
+        filtered_data = filtered_data.sort_values(['category', 'LTV', 'bill_count'], ascending=[True, False])
 
-    final_data = pd.DataFrame()
-    for category in new_category_values.keys():
-        final_data = pd.concat([final_data, filtered_data[filtered_data['category'] == category].head(new_category_values[category])])
+        final_data = pd.DataFrame()
+        for category in new_category_values.keys():
+            final_data = pd.concat([final_data, filtered_data[filtered_data['category'] == category].head(new_category_values[category])])
 
-    return final_data
+        return final_data
+
+    except Exception as e:
+        logging()
+        return
 
 
 def main():
-    new_customers_data = get_new_customers_data()
-    # new_customers_data = assign_category(new_customers_data)
-    # filtered_data = filter_based_on_category(new_customers_data)
-    # final_data = final_data_limited(filtered_data)
-    #return final_data
+    try:
+        new_customers_data = get_new_customers_data()
+        # new_customers_data = assign_category(new_customers_data)
+        # filtered_data = filter_based_on_category(new_customers_data)
+        # final_data = final_data_limited(filtered_data)
+        #return final_data
+        logger.info("New customers task done successfully")
 
+
+    except Exception as e:
+        logging()
+        return
